39b939b9f2e38c275301495fcb24cd44e14b60f1
sidonie <shelliednnguyen@gmail.com>
2021-04-15 10:41:08 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require( 'express' );
+const mongoose = require( 'mongoose' );
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static( 'public' ));
+
+app.use(require( './routes' ));
+
+/*
+mongoose.connect() tells Mongoose which database we want to connect to.
+If the environment variable MONGODB_URI exists, like on Heroku when we
+deploy later, it will use that. Otherwise, it will short-circuit to the
+local MongoDB server's database at mongodb://localhost/social-network.
+MongoDB will find and connect to the database if it exists or create the
+database if it doesn't.
+*/
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set( 'debug', true );
+
+app.listen( PORT, () => console.log(`🌍 Connected on localhost:${PORT}` ));
